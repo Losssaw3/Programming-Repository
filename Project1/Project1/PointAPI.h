1cedac101f0307013c923575b05abf9428d42ae0
losssaw3 <baylo.04@mail.ru>
2023-03-15 20:05:41 +0300

    try 3. тогда совсем тривиальная структура получается))

---
+++ Project1/Project1/PointAPI.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include <iostream>
+using namespace std;
+/*
+@brief Point class 
+*/
+struct Point {
+
+	double abscissa;
+	double ordinate;
+	/*
+	@brief Initializes a new instance of the 'Point' class (abscissa , ordinate)
+	@param x - abscissa
+	@param y - ordinate
+	*/
+	Point(const double x ,const double y);
+
+	/*
+	@brief Initializes a new instance of the 'Point' class (0 , 0)
+	*/
+	Point();
+
+	/*
+	@brief function overriding the output stream
+	@param out outstream
+	@param point point
+	@return returns point
+	*/
+	friend std::ostream& operator<< (std::ostream& out, const Point& point);
+
+};
