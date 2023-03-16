2808b2a1b83eeddb7f03e1577763df790971939a
losssaw3 <baylo.04@mail.ru>
2023-03-16 19:12:42 +0300

    try4

--- Project1/Project1/PointAPI.h
+++ Project1/Project1/PointAPI.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 #include <iostream>
 using namespace std;
+
 /*
 @brief Point class 
 */
@@ -8,24 +9,20 @@ struct Point {
 
 	double abscissa;
 	double ordinate;
+
 	/*
-	@brief Initializes a new instance of the 'Point' class (abscissa , ordinate)
+	@brief Initializes a new instance of the 'Point' class
 	@param x - abscissa
 	@param y - ordinate
 	*/
-	Point(const double x ,const double y);
-
-	/*
-	@brief Initializes a new instance of the 'Point' class (0 , 0)
-	*/
-	Point();
+	Point(const double x = 0,const double y = 0);
 
 	/*
 	@brief function overriding the output stream
 	@param out outstream
 	@param point point
-	@return returns point
+	@return returns output stream
 	*/
-	friend std::ostream& operator<< (std::ostream& out, const Point& point);
+	friend ostream& operator<< (ostream& out, const Point& point);
 
 };
